1a286f11cfcebf0e837bfa2f03b9683e8ade118e
fika4life <fistewie@gmail.com>
2015-04-10 12:36:53 +0200

    adds search vc

---
+++ SearchViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SearchViewController.swift
+//  
+//
+//  Created by Fiona on 10/04/15.
+//
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var courseField: UITextField!
+    
+    @IBOutlet weak var teacherField: UITextField!
+    
+    @IBOutlet weak var distanceField: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
