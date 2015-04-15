ef5fb94d80430a48154e8498753c03ac20409f5b
fika4life <fistewie@gmail.com>
2015-04-15 13:55:41 +0200

    Adds searchResults

---
+++ Kurskollen/SearchResultsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchResultsViewController.swift
+//  Kurskollen
+//
+//  Created by Fiona on 15/04/15.
+//  Copyright (c) 2015 Fiona. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
