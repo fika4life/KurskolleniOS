9f88a500d7389c6d2165fde3837eca8b3defd8be
Fredrik Wallén <fwallen@turkos-03.csc.kth.se>
2015-04-20 17:26:33 +0200

    Made the SearchCourseDetailViewController display more data and added tableview for it

---
+++ Kurskollen/ReviewListTableCell.swift
@@ -0,0 +1,33 @@
+//
+//  ReviewListTableCell.swift
+//  Kurskollen
+//
+//  Created by Fredrik Wallén on 20/04/15.
+//  Copyright (c) 2015 Fiona. All rights reserved.
+//
+
+import UIKit
+
+class ReviewListTableCell: UITableViewCell {
+
+    @IBOutlet weak var headerText: UILabel!
+    
+    @IBOutlet weak var reviewerNameText: UILabel!
+    
+    @IBOutlet weak var reviewText: UILabel!
+    
+    @IBOutlet weak var timeField: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.headerText.numberOfLines = 0
+        self.headerText.sizeToFit()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Kurskollen/SearchCourseDetailViewController.swift
+++ Kurskollen/SearchCourseDetailViewController.swift
@@ -16,12 +16,20 @@ class SearchCourseDetailViewController: UIViewController {
     @IBOutlet weak var credits: UILabel!
     @IBOutlet weak var online: UILabel!
     @IBOutlet weak var teacher: UIButton!
+    
+    self.courseData:JSON?
 
     @IBAction func writeReview(sender: AnyObject) {
     }
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        course.text = courseData["name"].string
+        school.text = courseData["school"].string;
+        credits.text = String(courseData["credits"].int)
+        online.text = courseData["online"].boolean ? "Distans" : "Ortsbunden"
+        
         
         // Do any additional setup after loading the view.
     }
