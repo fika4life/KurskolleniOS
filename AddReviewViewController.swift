ddb417ef63a841a1283ddc40f43996d5ec9dbba5
Fredrik Wallén <fwallen@turkos-06.csc.kth.se>
2015-05-05 13:54:40 +0200

    Started AddReview

--- AddReviewViewController.swift
+++ AddReviewViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class AddReviewViewController: UIViewController {
+    
+    var courseId : Int?
     
     @IBOutlet weak var reviewTeacher: UITextField!
 
@@ -17,6 +21,31 @@ class AddReviewViewController: UIViewController {
     @IBOutlet weak var rating: UITextField!
    
     @IBAction func Done(sender: AnyObject) {
+        let text = reviewText.text
+        let theRating = rating.text
+        let teacher = reviewTeacher.text
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        let email = userDefaults.stringForKey(globalConstants.emailMemoryKey)
+        let loginSession = userDefaults.stringForKey(globalConstants.loginSessionMemoryKey)
+        
+        let parameters = ["email" : email, "loginsession" : loginSession, "rating" : theRating, "courseid" : self.courseId!, "teacherid" : teacher, "text" : text]
+        
+        Alamofire.request(.POST, globalConstants.URL + "create-review", parameters: parameters)
+            .validate()
+            .responseJSON{(request, response, data, error) in
+                self.view.endEditing(true)
+                if(error != nil){
+                    var alert = UIAlertController(title: "Communication error", message: "Could not communicate with server", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+                else{
+                    
+                }
+                
+        }
+        
     }
     
     override func viewDidLoad() {
